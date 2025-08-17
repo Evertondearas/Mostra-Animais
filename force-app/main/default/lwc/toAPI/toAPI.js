89784a17b56e509fc39cced6c2071ce5189ce0c0
Evertondearas <everton.araujogta@gmail.com>
2025-08-17 02:00:29 -0300

    Adiciona LWC toAPI e estrutura inicial do projeto SFDX

---
+++ force-app/main/default/lwc/toAPI/toAPI.js
@@ -0,0 +1,23 @@
+import { LightningElement, wire } from 'lwc';
+import getAnimalList from '@salesforce/apex/ConsumindoApiAnimais.usoLWC';
+
+export default class ToAPI extends LightningElement {
+    animals;
+    error;
+
+    connectedCallback() {
+        this.loadAnimals();
+    }
+
+    async loadAnimals() {
+        try {
+            this.animals = await getAnimalList();
+            this.error = undefined;
+            console.log('Animais: '+this.animals);
+        } catch (error) {
+            this.animals = undefined;
+            this.error = error;
+            console.log('Erro:'+error.message);
+        }
+    }
+}
